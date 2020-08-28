9117397cb0b3dfbe74c94fa679bd30789807c42b
jdhofmann <justindhofmann@gmail.com>
2020-08-27 23:14:20 -0400

    added some bonus methods that might be horribly wrong"

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Waiter
   attr_accessor :name, :yrs_experience
 
@@ -26,6 +28,29 @@ class Waiter
       meal_a.tip <=> meal_b.tip
     end
     best_tip_meal.customer
+    # binding.pry
   end
 
+  def worst_tipper
+    worst_tip_meal = meals.min do |meal_a , meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+    worst_tip_meal.customer
+  end
+
+  def most_frequent_customer
+    # This is a guess, I'm not sure if I can write a test for this #
+    customer_meal_number = {}
+    
+    meals.each do |meal|
+    if customer_meal_number.key?(meal.customer)
+      customer_meal_number[meal.customer] += 1
+    elsif
+      customer_meal_number[meal.customer] = 1
+    end
+    most_frequent = customer_meal_number.max | cus_a , cus_b |
+      :cus_a <=> :cus_b
+    end
+    most_frequent
+  end
 end
